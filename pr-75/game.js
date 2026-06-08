536e5677146c961db58a75c53f1a1516df2f5e1c
iYung <iYung@users.noreply.github.com>
2026-06-08 17:36:15 +0000

    deploy: ea683500a267834ca038fa54bb71d1033796b959

--- pr-75/game.js
+++ pr-75/game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"6165c2e4-557f-4cef-9dc2-ba7db81cdc51","remote_package_size":19207517,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":19207517,"audio":false}]});
+  loadPackage({"package_uuid":"8ab0e35f-73f9-4577-932e-39b594387956","remote_package_size":19207517,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":19207517,"audio":false}]});
 
 })();
