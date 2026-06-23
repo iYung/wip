f093330595c8e4197cd808d4a8eae95bbdea7c1c
Ivan Yung <ivankhyung@gmail.com>
2026-06-23 19:33:39 -0400

    win scene

--- lua/game/scenes/win_scene.lua
+++ lua/game/scenes/win_scene.lua
@@ -1,6 +1,9 @@
 local Scene  = require("lua/core/scene")
+local WinBg  = require("lua/game/shaders/win_bg")
 local A      = require("lua/game/assets")
 local config = require("lua/game/config")
+
+local ROTATE_SPEED = 0.3
 
 local WinScene = setmetatable({}, { __index = Scene })
 WinScene.__index = WinScene
@@ -12,14 +15,23 @@ function WinScene.new(game_state, input, scene_manager, store_scene)
     self.input          = input
     self.scene_manager  = scene_manager
     self.store_scene    = store_scene
+    self._time          = 0
     return self
 end
 
-function WinScene:on_enter() end
+function WinScene:on_enter()
+    self._time = 0
+    if love.filesystem.getInfo("assets/images/start_pattern.png") then
+        local pat = love.graphics.newImage("assets/images/start_pattern.png")
+        pat:setWrap("repeat", "repeat")
+        self._img_pattern = pat
+    end
+end
 
 function WinScene:on_exit() end
 
 function WinScene:update(dt)
+    self._time = self._time + dt
     if self.input:pressed("cancel") then
         self.scene_manager:switch(self.store_scene)
     end
@@ -29,7 +41,11 @@ function WinScene:draw()
     local img    = A.win_scene
     local iw, ih = img:getDimensions()
     love.graphics.setColor(1, 1, 1, 1)
+    if self._img_pattern then
+        WinBg.apply(self._img_pattern, img, self._time * ROTATE_SPEED)
+    end
     love.graphics.draw(img, 0, 0, 0, config.LOGICAL_W / iw, config.LOGICAL_H / ih)
+    if self._img_pattern then WinBg.clear() end
 end
 
 return WinScene
